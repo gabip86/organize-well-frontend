405857629acaa9f2f47b405b2e704bdf9627eb81
Gabriella Pál <gabriella.pal@ericsson.com>
2022-02-16 08:05:16 +0100

    Create ListExpenseComponent

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import ListExpenseComponent from "./components/ListExpenseComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <Router>
+              <HeaderComponent />
+              <div className="container">
+                  <Switch>
+                      <Route path="/*" component={ListExpenseComponent}/>
+                      <Route path="/expenses" component={ListExpenseComponent}/>
+                      <ListExpenseComponent />
+                  </Switch>
+              </div>
+              <FooterComponent />
+          </Router>
+      </div>
   );
 }
 
